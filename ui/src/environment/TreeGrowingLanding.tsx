43f02b13a3fc59c6dd571d36748a0858fbb52d1c
geoffreynyaga <geoffreynyagak@gmail.com>
2022-09-26 22:29:05 +0300

    final csv

--- ui/src/environment/TreeGrowingLanding.tsx
+++ ui/src/environment/TreeGrowingLanding.tsx
@@ -13,22 +13,25 @@
  * Copyright (c) 2022 Swift Lab Limited.
  */
 
-import React, { useEffect, useState } from "react";
-import { useFilters, useTable } from "react-table";
+import React, { useEffect, useState } from 'react';
+import { useFilters, useSortBy, useTable } from 'react-table';
 
-import { IMonthlySales } from "../../typings/LMETypes";
+import { IMonthlySales } from '../../typings/LMETypes';
+// import JsPDF from "jspdf";
+import Papa from 'papaparse';
+import { useExportData } from 'react-table-plugins';
 
 function TreeGrowingLanding() {
   const [lme, setLme] = useState<null | IMonthlySales[]>(null);
-  const [partnerInput, setPartnerInput] = useState("");
-  const [factoryInput, setFactoryInput] = useState("");
-  const [venueInput, setVenueInput] = useState("");
+  const [partnerInput, setPartnerInput] = useState('');
+  const [factoryInput, setFactoryInput] = useState('');
+  const [venueInput, setVenueInput] = useState('');
 
   async function fetchLMEs() {
-    await fetch("/api/v1/environment/tree-growing/")
+    await fetch('/api/v1/environment/tree-growing/')
       .then((response) => response.json())
       .then((data) => {
-        console.log(data, "data");
+        // console.log(data, 'data');
         if (data.length > 0) {
           setLme(data);
         }
@@ -44,37 +47,37 @@ function TreeGrowingLanding() {
   const columns = React.useMemo(
     () => [
       {
-        Header: "Partner",
-        accessor: "partner", // accessor is the "key" in the data
+        Header: 'Partner',
+        accessor: 'partner' // accessor is the "key" in the data
       },
       {
-        Header: "Factory",
-        accessor: "factory",
+        Header: 'Factory',
+        accessor: 'factory'
       },
       {
-        Header: "Venue",
-        accessor: "venue",
+        Header: 'Venue',
+        accessor: 'venue'
       },
       {
-        Header: "Trees Planted",
-        accessor: "trees_planted",
+        Header: 'Trees Planted',
+        accessor: 'trees_planted'
       },
       {
-        Header: "Date",
-        accessor: "date",
+        Header: 'Date',
+        accessor: 'date'
       },
       {
-        Header: "Indigenous Trees",
-        accessor: "indigenous_trees",
+        Header: 'Indigenous Trees',
+        accessor: 'indigenous_trees'
       },
       {
-        Header: "Exotic Trees",
-        accessor: "exotic_trees",
+        Header: 'Exotic Trees',
+        accessor: 'exotic_trees'
       },
       {
-        Header: "Fruit Trees",
-        accessor: "fruit_trees",
-      },
+        Header: 'Fruit Trees',
+        accessor: 'fruit_trees'
+      }
     ],
     []
   );
@@ -86,34 +89,66 @@ function TreeGrowingLanding() {
     rows,
     prepareRow,
     setFilter,
-  } = useTable({ columns, data }, useFilters);
+    exportData
+  } = useTable({ columns, data, getExportFileBlob }, useFilters, useExportData);
 
   // Update the state when input changes
 
   const handlePartnerChange = (e: any) => {
     const value = e.target.value || undefined;
-    setFilter("partner", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+    setFilter('partner', value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
     setPartnerInput(value);
   };
 
   const handleFactoryFilterChange = (e: any) => {
     const value = e.target.value || undefined;
-    setFilter("factory", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+    setFilter('factory', value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
     setFactoryInput(value);
   };
 
   const handleVenueChange = (e: any) => {
     const value = e.target.value || undefined;
-    setFilter("venue", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+    setFilter('venue', value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
     setVenueInput(value);
   };
 
+  function getExportFileBlob({ columns, data, fileType, fileName }) {
+    if (fileType === 'csv') {
+      // CSV example
+      const headerNames = columns.map((col) => col.exportValue);
+      const csvString = Papa.unparse({ fields: headerNames, data });
+      return new Blob([csvString], { type: 'text/csv' });
+    }
+    //PDF example
+    //if (fileType === "pdf") {
+    //  const headerNames = columns.map((column) => column.exportValue);
+    //  const doc = new JsPDF();
+    //  doc.autoTable({
+    //    head: [headerNames],
+    //    body: data,
+    //    margin: { top: 20 },
+    //    styles: {
+    //      minCellHeight: 9,
+    //      halign: "left",
+    //      valign: "center",
+    //      fontSize: 11,
+    //    },
+    //  });
+    //  doc.save(`${fileName}.pdf`);
+    //
+    //  return false;
+    //}
+
+    // Other formats goes here
+    return false;
+  }
+
   return (
-    <div className="flex flex-col items-center flex-1 pt-0 mx-auto bg-gray-100 ">
+    <div className='flex flex-col items-center flex-1 pt-0 mx-auto bg-gray-100 '>
       {/* <!-- header --> */}
 
-      <div className="flex flex-row items-center w-11/12 py-2 mt-2 mb-4 bg-gray-200 rounded-lg shadow justify-evenly ">
-        <div className="flex flex-row justify-center w-4/12 ">
+      <div className='flex flex-row items-center w-11/12 py-2 mt-2 mb-4 bg-gray-200 rounded-lg shadow justify-evenly '>
+        <div className='flex flex-row justify-center w-4/12 '>
           {/* <a
             href="/environment/tree-growing/create/"
             className="px-4 py-2 bg-gray-300 border-2 rounded-lg sm:text-sm"
@@ -122,83 +157,83 @@ function TreeGrowingLanding() {
           </a> */}
 
           <a
-            href="/environment/tree-growing/create/"
-            className="inline-flex overflow-hidden text-white bg-gray-900 rounded group"
+            href='/environment/tree-growing/create/'
+            className='inline-flex overflow-hidden text-white bg-gray-900 rounded group'
           >
-            <span className="px-3.5 py-2 text-white bg-purple-500 group-hover:bg-purple-600 flex items-center justify-center">
+            <span className='px-3.5 py-2 text-white bg-purple-500 group-hover:bg-purple-600 flex items-center justify-center'>
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
+                xmlns='http://www.w3.org/2000/svg'
+                className='w-6 h-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth='2'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 4v16m8-8H4"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M12 4v16m8-8H4'
                 />
               </svg>
             </span>
-            <span className="pl-4 pr-5 py-2.5">New Tree Growing Activity</span>
+            <span className='pl-4 pr-5 py-2.5'>New Tree Growing Activity</span>
           </a>
         </div>
-        <div className="flex flex-row justify-center w-4/12 ">
-          <button className="px-4 py-2 text-red-500 rounded-lg bg-gray-50 ">
-            <h1 className="text-center h6">Tree Growing List</h1>
+        <div className='flex flex-row justify-center w-4/12 '>
+          <button className='px-4 py-2 text-red-500 rounded-lg bg-gray-50 '>
+            <h1 className='text-center h6'>Tree Growing List</h1>
           </button>
         </div>
-        <div className="flex flex-row justify-center w-4/12 ">
-          <button className="px-4 py-2 bg-gray-300 border-2 rounded-lg ">
+        <div className='flex flex-row justify-center w-4/12 '>
+          {/* <button className="px-4 py-2 bg-gray-300 border-2 rounded-lg ">
             Report
-          </button>
+          </button> */}
         </div>
       </div>
       {/* <!-- end header --> */}
 
-      <div className="w-full mx-2 mb-6">
+      <div className='w-full mx-2 mb-6'>
         {lme !== null ? (
           <>
-            <div className="grid grid-cols-3 gap-4 px-4 py-2 mx-2 mb-4 sm:grid-cols-2">
-              <div className="flex flex-col justify-around">
+            <div className='grid grid-cols-3 gap-4 px-4 py-2 mx-2 mb-4 sm:grid-cols-2'>
+              <div className='flex flex-col justify-around'>
                 <div>
                   <p>Partner Name</p>
                 </div>
-                <div className="mt-2">
+                <div className='mt-2'>
                   <input
-                    type="text"
-                    className="w-full p-2 text-sm border-2 rounded-lg"
-                    placeholder="Search Partner"
+                    type='text'
+                    className='w-full p-2 text-sm border-2 rounded-lg'
+                    placeholder='Search Partner'
                     value={partnerInput}
                     onChange={handlePartnerChange}
                   />
                 </div>
               </div>
-              <div className="flex flex-col justify-around">
+              <div className='flex flex-col justify-around'>
                 <div>
                   <p>Factory</p>
                 </div>
-                <div className="mt-2">
+                <div className='mt-2'>
                   <input
-                    type="text"
-                    className="w-full p-2 text-sm border-2 rounded-lg"
-                    placeholder="Search Factory"
+                    type='text'
+                    className='w-full p-2 text-sm border-2 rounded-lg'
+                    placeholder='Search Factory'
                     value={factoryInput}
                     onChange={handleFactoryFilterChange}
                   />
                 </div>
               </div>
 
-              <div className="flex flex-col justify-around">
+              <div className='flex flex-col justify-around'>
                 <div>
                   <p>Venue</p>
                 </div>
-                <div className="mt-2">
+                <div className='mt-2'>
                   <input
-                    type="text"
-                    className="w-full p-2 text-sm border-2 rounded-lg"
-                    placeholder="Search Venue"
+                    type='text'
+                    className='w-full p-2 text-sm border-2 rounded-lg'
+                    placeholder='Search Venue'
                     value={venueInput}
                     onChange={handleVenueChange}
                   />
@@ -206,10 +241,28 @@ function TreeGrowingLanding() {
               </div>
             </div>
             <hr />
+            <div className='flex flex-row items-center justify-center w-full py-4'>
+              <button
+                className='h-10 px-4 py-1 mx-2 bg-gray-200 rounded-md shadow-md '
+                onClick={() => {
+                  exportData('csv', true);
+                }}
+              >
+                Export All as CSV
+              </button>
+              <button
+                className='h-10 px-4 py-1 mx-2 bg-gray-200 rounded-md shadow-md '
+                onClick={() => {
+                  exportData('csv', false);
+                }}
+              >
+                Export Current View as CSV
+              </button>
+            </div>
 
             <table
               {...getTableProps()}
-              className="w-full text-left table-fixed hover:table-auto"
+              className='w-full text-left table-fixed hover:table-auto'
             >
               {/* // Input element */}
 
@@ -221,13 +274,13 @@ function TreeGrowingLanding() {
                         {...column.getHeaderProps()}
                         style={{
                           // borderBottom: "solid 3px red",
-                          background: "aliceblue",
+                          background: 'aliceblue'
                           // color: "black",
                           // fontWeight: "bold",
                         }}
-                        className="py-2 text-xs text-center text-red-500 border-b-4 border-r border-b-black"
+                        className='py-2 text-xs text-center text-red-500 border-b-4 border-r border-b-black'
                       >
-                        {column.render("Header")}
+                        {column.render('Header')}
                       </th>
                     ))}
                   </tr>
@@ -242,9 +295,9 @@ function TreeGrowingLanding() {
                         return (
                           <td
                             {...cell.getCellProps()}
-                            className="px-1 py-2 text-xs text-center border border-l-0 "
+                            className='px-1 py-2 text-xs text-center border border-l-0 '
                           >
-                            {cell.render("Cell")}
+                            {cell.render('Cell')}
                           </td>
                         );
                       })}
@@ -255,16 +308,16 @@ function TreeGrowingLanding() {
             </table>
           </>
         ) : (
-          <div className="flex flex-col items-center justify-center w-full py-4 bg-red-200 h-72">
-            <h1 className="tracking-wider text-center text-gray-700 md:text-2xl text-md">
+          <div className='flex flex-col items-center justify-center w-full py-4 bg-red-200 h-72'>
+            <h1 className='tracking-wider text-center text-gray-700 md:text-2xl text-md'>
               No Tree Growing Activity Recorded yet!
             </h1>
             <hr />
-            <p className="mt-6 tracking-wider text-gray-700 text-md">
+            <p className='mt-6 tracking-wider text-gray-700 text-md'>
               Add at least one activity to unlock this page.
             </p>
-            <a href="/environment/tree-growing/create/" className="mt-6">
-              <button className="px-6 py-2 bg-red-600 rounded-lg shadow-lg ">
+            <a href='/environment/tree-growing/create/' className='mt-6'>
+              <button className='px-6 py-2 bg-red-600 rounded-lg shadow-lg '>
                 Add new Tree Growing Activity
               </button>
             </a>
